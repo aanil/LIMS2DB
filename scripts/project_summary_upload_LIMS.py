a352576c40777b75e4a9043a60ca899977b9854f
Denis Moreno <denis.moreno@scilifelab.se>
2015-08-07 13:41:54 +0200

    rewriting the error stack

--- scripts/project_summary_upload_LIMS.py
+++ scripts/project_summary_upload_LIMS.py
@@ -161,7 +161,7 @@ def processPSUL(options, queue, logqueue):
                     P.handle_project()
                 except :
                     error=sys.exc_info()
-                    proclog.error("{0}:{1}\n{2}".format(error[0], error[1], traceback.print_tb(error[2])))
+                    proclog.error("{0}:{1}\n{2}".format(error[0], error[1], traceback.extract_tb(error[2])))
                 try:
                     os.remove(lockfile)
                 except:
