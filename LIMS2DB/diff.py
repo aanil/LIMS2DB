3e1b4c1235c9d7a7b4615de64aa8973079b9f579
Johannes Alneberg <alneberg@kth.se>
2021-05-12 07:49:32 +0200

    Cleanup of the diff-comparison script

--- LIMS2DB/diff.py
+++ LIMS2DB/diff.py
@@ -1,15 +1,13 @@
 
-from genologics.config import BASEURI, USERNAME, PASSWORD
-from genologics.lims import Lims
 from LIMS2DB.utils import setupLog
-from genologics_sql.utils import *
+from LIMS2DB.classes import ProjectSQL
+from genologics_sql.utils import get_session, get_configuration
 
-def diff_project_objects(pj_id, couch, logfile, new=True):
+
+def diff_project_objects(pj_id, couch, logfile):
 
     proj_db = couch['projects']
-    samp_db = couch['samples']
     log = setupLog('diff - {}'.format(pj_id), logfile)
-    lims = Lims(BASEURI, USERNAME, PASSWORD)
 
     view = proj_db.view('projects/lims_followed')
 
@@ -18,59 +16,46 @@ def diff_project_objects(pj_id, couch, logfile, new=True):
     except (KeyError, IndexError):
         log.error("No such project {}".format(pj_id))
 
-
     old_project = proj_db.get(old_project_couchid)
     old_project.pop('_id', None)
     old_project.pop('_rev', None)
     old_project.pop('modification_time', None)
     old_project.pop('creation_time', None)
 
-    if new:
-        from LIMS2DB.classes import ProjectSQL
-        session=get_session()
-        host=get_configuration()['url']
-        new_project=ProjectSQL(session, log, pj_id, host, couch)
-    else:
-        import LIMS2DB.objectsDB.objectsDB as DB
-        new_project = DB.ProjectDB(lims, pj_id, samp_db, log)
+    session = get_session()
+    host = get_configuration()['url']
+    new_project = ProjectSQL(session, log, pj_id, host, couch)
 
-    fediff=diff_objects(old_project, new_project.obj)
+    fediff = diff_objects(old_project, new_project.obj)
 
     return (fediff, old_project, new_project.obj)
 
 
-
-
-
-
 def diff_objects(o1, o2, parent=''):
-    diffs={}
+    diffs = {}
 
     for key in o1:
         if key in o2:
             if isinstance(o1[key], dict):
-                more_diffs=diff_objects(o1[key], o2[key], "{} {}".format(parent,key))
+                more_diffs = diff_objects(o1[key], o2[key], "{} {}".format(parent, key))
                 diffs.update(more_diffs)
-
             else:
-                if o1[key]!= o2[key]:
-                    diffs["{} {}".format(parent, key)]=[o1[key], o2[key]]
+                if o1[key] != o2[key]:
+                    diffs["{} {}".format(parent, key)] = [o1[key], o2[key]]
 
         else:
             if o1[key]:
-                diffs["key {} {}".format(parent, key)]=[o1[key], "missing"]
+                diffs["key {} {}".format(parent, key)] = [o1[key], "missing"]
 
     for key in o2:
         if key not in o1 and o2[key]:
-            diffs["key {} {}".format(parent, key)]=["missing", o2[key]]
-
+            diffs["key {} {}".format(parent, key)] = ["missing", o2[key]]
 
     return diffs
 
 
 if __name__ == "__main__":
-
-    a={'a' : 1, 'b': 2, 'c':{'d':3, 'e':{'f':5}}}
-    b={'a' : 1, 'b': 7, 'c':{'d':4, 'e':{'f':4}}}
-    diffs=diff_objects(a,b)
+    a = {'a': 1, 'b': 2, 'c': {'d': 3, 'e': {'f': 5}}}
+    b = {'a': 1, 'b': 7, 'c': {'d': 4, 'e': {'f': 4}}}
+    diffs = diff_objects(a, b)
     print(diffs)
--- scripts/run_diff_with_DB.py
+++ scripts/run_diff_with_DB.py
@@ -14,53 +14,45 @@ def write_results_to_file(diffs, args):
             for d in diffs[p]:
                 f.write(" {} : was {}, is {}\n".format(d, diffs[p][d][0], diffs[p][d][1]))
 
+
 def main(args):
     couch = load_couch_server(args.conf)
 
     if args.pj_id:
-        diffs=df.diff_project_objects(args.pj_id, couch, args.log)
+        diffs = df.diff_project_objects(args.pj_id, couch, args.log)
 
     elif args.random:
         random.seed()
-        closed_ids=[]
+        closed_ids = []
         proj_db = couch['projects']
         view = proj_db.view('project/summary')
         for row in view[['closed', '']:['closed', 'ZZZZZZZZ']]:
-
-            if row.value.get('open_date', '0')>'2014-06-01':
+            if row.value.get('open_date', '0') > '2014-06-01':
                 closed_ids.append(row.key[1])
-        nb=int(len(closed_ids)/10)
-        picked_ids=random.sample(closed_ids, nb)
+        nb = int(len(closed_ids)/10)
+        picked_ids = random.sample(closed_ids, nb)
         for one_id in picked_ids:
-            diffs=df.diff_project_objects(one_id, couch, args.log)
+            diffs = df.diff_project_objects(one_id, couch, args.log)
     else:
         proj_db = couch['projects']
         view = proj_db.view('project/project_id')
         for row in view:
-            diffs=df.diff_project_objects(row.key, couch, args.log)
+            diffs = df.diff_project_objects(row.key, couch, args.log)
 
     write_results_to_file(diffs, args)
 
 
-
-
-
-
 if __name__ == "__main__":
-
-
-    
     parser = argparse.ArgumentParser(description='Compare the results of the installed PSUL with the contents of the DB')
     parser.add_argument('--conf', dest="conf", default=os.path.expanduser("~/opt/config/post_process.yaml"),
-                               help='configuration file path. default is ~/opt/config/post_process.yaml')
+                        help='configuration file path. default is ~/opt/config/post_process.yaml')
     parser.add_argument('--log', '-l', dest="log", default=os.path.expanduser("~/psul_validation.log"),
-                               help='log file path. default is ~/psul_validation.log')
+                        help='log file path. default is ~/psul_validation.log')
     parser.add_argument('--result', '-r', dest="resultfile", default=os.path.expanduser("~/psul_validations/{}_psul_validation.out".format(datetime.datetime.now().isoformat())),
-                               help='validation output path. default is ~/psul_validations/{date}_psul_validation.out')
-    parser.add_argument('--project', '-p', dest='pj_id',help='project id to perform the check')
-    parser.add_argument('--randomsample', '-s', action="store_true", dest='random',help='pick a random subset of projects to check')
+                        help='validation output path. default is ~/psul_validations/{date}_psul_validation.out')
+    parser.add_argument('--project', '-p', dest='pj_id', help='project id to perform the check')
+    parser.add_argument('--randomsample', '-s', action="store_true", dest='random', help='pick a random subset of projects to check')
 
     args = parser.parse_args()
-
 
     main(args)
